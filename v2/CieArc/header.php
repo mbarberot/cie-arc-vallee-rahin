daaaf3b5b0b6eb5b1e7a6b2a206facb1c771b5ed
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com@a41b63c4-d419-d6a0-c3c4-c6fe0c211a36>
2013-05-01 19:39:29 +0000

    Premier layout

---
+++ v2/CieArc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head <?php language_attributes(); ?>>
+        <meta charset="<?php bloginfo('charset'); ?>" />
+        <title><?php bloginfo('name'); ?></title>
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+        <?php wp_head(); ?>        
+    </head>
+    <body>
+        <div class="wrap" >
+            <header>
+                <h1><a><?php bloginfo('name'); ?></a></h1>
+                <h2><?php bloginfo('description'); ?></h2>
+            </header>
+        </div>
