f2d5a02b5b8d218c9ed1a048bfb2d7ceb9c8869c
mathieu.barberot@gmail.com <mathieu.barberot@gmail.com@a41b63c4-d419-d6a0-c3c4-c6fe0c211a36>
2013-05-01 20:14:25 +0000

    Ajout d'une boucle secondaire dans la sidebar.php

--- v2/CieArc/sidebar.php
+++ v2/CieArc/sidebar.php
@@ -7,4 +7,14 @@
   <ul class="list">
     <?php wp_get_archives('type=monthly'); ?>
   </ul>
+  
+  <!-- Derniers articles -->
+<h4 class="section">Derniers articles</h4>
+<ul class="list">
+  <?php wp_reset_postdata();
+  query_posts('posts_per_page=5');
+  while (have_posts()) : the_post(); ?>
+    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+  <?php endwhile; ?>
+</ul>
 </div>
